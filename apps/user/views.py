e104b5916ea84563ac12af7524d219b98cde9da0
Your Name <you@example.com>
2017-11-09 12:46:52 +0530

    template changes and payment

--- apps/user/views.py
+++ apps/user/views.py
@@ -52,7 +52,8 @@ class ChargeView(StripeMixin, generic.FormView):
         form = self.form_class(request.POST)
         if (form.is_valid()):
             username = request.session['username']
-            q1 = User.objects.get(username=username)
+            email = form.cleaned_data.get('email')
+            q1 = User.objects.get(email=email)
             q = Profile.objects.get(user_id=q1.id)
             q.payed = True
             q.save()
